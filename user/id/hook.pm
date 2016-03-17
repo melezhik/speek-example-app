8c2aa2d4c0b04c8d73b55d6832b147304ae05bdc
Alexey Melezhik <melezhik@gmail.com>
2016-03-17 18:19:52 +0300

    up hook

--- user/id/hook.pm
+++ user/id/hook.pm
@@ -1,9 +1,8 @@
 
-if (http_method() eq 'GET'){
-    modify_resource(sub {
-        my $r  = shift;
-        my $id = module_variable('id');
-        s{/id}[/$id] for $r;
-        $r;
-    })
-}
+modify_resource(sub{
+    my $r = shift;
+    my $id = module_variable('id');
+    s{/id}[/$id] for $r;
+    $r;
+});
+
